814fdf619671b514bac8c0e685dbfc010956e689
sambhaji <kolate.sambhaji@gmail.com>
2018-01-30 13:01:30 +0530

    Added

---
+++ criscoconsulting/config/accounts.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Billing"),
+			"icon": "fa fa-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Entry of Receipt",
+					"description": _("Entry of Receipt"),
+				},
+				{
+					"type": "doctype",
+					"name": "Entry Of Payment",
+					"description": _("Entry Of Payment"),
+				},
+			]
+		},
+	]
