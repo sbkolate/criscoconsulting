0536ceeb34d64f8adff25b7e8cef9ad2b40f5dac
maheshlangotedp <mahesh@digitalprizmglobal.com>
2018-03-22 13:16:33 +0530

    update  material requset

--- criscoconsulting/custom_method.py
+++ criscoconsulting/custom_method.py
@@ -3,11 +3,14 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 def material_request_data(self,Document):
-	frappe.msgprint("Test")
-	query = frappe.db.sql("""select name, status
-			from `tabMaterial Request` WHERE material_request='{self.material_request}' """,as_dict=1)
-	for data in query :
-		new_data += data
-		frappe.msgprint(new_data)
-	self.doc_details="test"
+	# frappe.msgprint("Test")
+	query = frappe.db.sql("""select name, material_transfer_type
+			from `tabMaterial Transfer` 
+			WHERE material_request='{0}' """.format(self.material_request),as_dict=1)
+	
+	doc_details=""
+	for i in query :
+		doc_details += i.name+" - "+i.material_transfer_type+" \n"
+		
+	self.doc_details=doc_details
 
