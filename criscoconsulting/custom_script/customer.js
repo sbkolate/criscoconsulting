d6341a18b149c91b83fa99e72c744952c5eca8e5
sbkolate <kolate.sambhaji@gmail.com>
2018-03-28 14:41:20 +0530

    Update customer.js

--- criscoconsulting/custom_script/customer.js
+++ criscoconsulting/custom_script/customer.js
@@ -1,9 +1,1 @@
-frappe.ui.form.on("Customer", "refresh", function(frm) {cur_frm.fields_dict['customer_group'].get_query = function(doc) {
-        return {
-            filters: {
-                'is_group': 0
-           }
-		console.log("test");
-        }
-    };
-});
+
