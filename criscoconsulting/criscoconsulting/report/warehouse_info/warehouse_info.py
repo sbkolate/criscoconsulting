4f964e1dfe40029018aa5a97b667f292721df91e
sambhaji <kolate.sambhaji@gmail.com>
2018-03-29 21:06:07 +0530

    added

--- criscoconsulting/criscoconsulting/report/warehouse_info/warehouse_info.py
+++ criscoconsulting/criscoconsulting/report/warehouse_info/warehouse_info.py
@@ -13,28 +13,61 @@ def execute(filters=None):
 	new_data=[]
 	for i in stock:
 		# print("\n",i[3])
-		new_data.append([i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8],i[9],i[10]])
+		i2=0
+		i3=0
+		i6=0
+		i5=0
+		i9=0
+		i8=0
+		if i[3] == None:
+			i3=0
+		else:
+			i3=i[3]
+		if i[3] == None:
+			i2=0
+		else:
+			i2=i[3]
+
+		if i[6] == None:
+			i6=0
+		else:
+			i6=i[6]
+		if i[5] == None:
+			i5=0
+		else:
+			i5=i[5]
+
+		if i[9] == None:
+			i9=0
+		else:
+			i9=i[9]
+		if i[8] == None:
+			i8=0
+		else:
+			i8=i[8]
+
+		i10 = i2+i5+i8
+
+
+
+		new_data.append(['i[0]',i[1],i2,i3,i[4],i5,i6,i[7],i8,i9,i10])
 	
-	new_list = []
 
-	for i in new_data:
-		new_list.append([i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8],i[9],i[10]])
-
-	stock=new_list
-	return columns, new_data
+	stock=new_data
+	return columns, stock
 
 def get_columns():
 	columns = [
 		_("Item Code") + ":Link/Item:150",
 		("Main - Riyadh") + ":Link/Item:150",
 		_("Stock-Riyadh") + ":Float:120",
-		("Valuation Rate") + ":Float:120",
+		("Valuation Rate MR") + ":Float:120",
 		_("Main-Al-Khobar - SAM") + ":Data:150",
 		_("Stock-MAK") + ":Float:120",
-		("Valuation Rate") + ":Float:120",
+		("Valuation Rate MAK") + ":Float:120",
 		_("Main -Jeddah - SAM") + ":Data:150",
 		_("Stock-MJ1") + ":Float:120",
-		("Valuation Rate") + ":Float:120",
+		("Valuation Rate MJ") + ":Float:120",
 		_("Total QTY") + ":Float:120",
 
 
@@ -105,17 +138,7 @@ def get_total_stock(filters):
 			    	where b.warehouse = 'Main-Jeddah - SAM' 
 			    	and b.item_code=tb.item_code limit 1)
 			    ELSE 0
-			END as act_w22,
-			CASE
-			    WHEN 1=1
-			    THEN (select sum(b.actual_qty)
-			    	from tabBin b 
-			    	where b.item_code=tb.item_code
-			    	and (b.warehouse = 'Main-Al-Khobar - SAM' 
-			    	OR b.warehouse = 'Main-Riyadh - SAM' 
-			    	or b.warehouse = 'Main-Jeddah - SAM'))
-			    ELSE 0
-			END as act_w11
+			END as act_w22
 			from tabBin tb group by tb.item_code""")
 
 
